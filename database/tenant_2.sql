b1ab6c78612cc7b665c2fc625a2ce8574aa31087
Ralphive <ralphrpo@gmail.com>
2019-05-03 18:45:44 +0100

    typo

--- database/tenant_2.sql
+++ database/tenant_2.sql
@@ -8,5 +8,5 @@ create table tenant_2."Tasks"
 	"TaskTypeId" int not null,
 	"UserId" int not null
 );
-INSERT INTO tenant_2."Tasks" ("Id", "Description", "Done", "TaskTypeId", "UserId") VALUES (1, 'Create SCHEMA schemas', false, 1, 1);
-INSERT INTO tenant_2."Tasks" ("Id", "Description", "Done", "TaskTypeId", "UserId") VALUES (2, 'Create express app', false, 2, 1);
+INSERT INTO tenant_2."Tasks" ("Id", "Description", "Done", "TaskTypeId", "UserId") VALUES (1, 'Create SCHEMA schemas', false, 1, 2);
+INSERT INTO tenant_2."Tasks" ("Id", "Description", "Done", "TaskTypeId", "UserId") VALUES (2, 'Create express app', false, 2, 2);
